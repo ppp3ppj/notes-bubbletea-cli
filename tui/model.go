f8303e5a9314ff505c594f65ed148ff833364262
ppp3ppj <ppp.3ppj@gmail.com>
2024-11-21 22:09:23 +0700

    add render lipgloss

--- tui/model.go
+++ tui/model.go
@@ -4,43 +4,87 @@ import (
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
 )
 
 const (
-    listView uint = iota
-    titleView
-    bodyView
+	listView uint = iota
+	titleView
+	bodyView
 )
 
 type model struct {
-    state uint
-    store *Store
-    notes []Note
-    currNote Note
-    listIndex int
-    // store Store
-    // textarea.Model
-    // ...
+	state     uint
+	store     *Store
+	notes     []Note
+	currNote  Note
+	listIndex int
+    textArea textarea.Model
+    textInput textinput.Model
 }
 
 func NewModel(store *Store) model {
-    notes, err := store.GetNotes()
-    if err != nil {
-        log.Fatalf("unable to get notes: %v", err)
-    }
+	notes, err := store.GetNotes()
+	if err != nil {
+		log.Fatalf("unable to get notes: %v", err)
+	}
 
-    return model{
-        state: listView,
-        store: store,
-        notes: notes,
-    }
+	return model{
+		state: listView,
+		store: store,
+		notes: notes,
+        textArea: textarea.New(),
+        textInput: textinput.New(),
+	}
 }
 
 func (m model) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    return m, nil
-}
+    var (
+        cmds []tea.Cmd
+        cmd tea.Cmd
+    )
 
+    m.textInput, cmd = m.textInput.Update(msg)
+    cmds = append(cmds, cmd)
+
+    m.textArea, cmd = m.textArea.Update(msg)
+    cmds = append(cmds, cmd)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		key := msg.String() //up, down, etc ...
+		switch m.state {
+		case listView:
+			switch key {
+			case "q":
+				return m, tea.Quit
+            case "n":
+                m.textInput.SetValue("")
+                m.textInput.Focus()
+                m.currNote = Note{}
+                m.state = titleView
+            case "up", "k":
+                if m.listIndex > 0 {
+                    m.listIndex--
+                }
+            case "down", "j":
+                if m.listIndex < len(m.notes) - 1 {
+                    m.listIndex++
+                }
+            case "enter":
+                m.currNote = m.notes[m.listIndex]
+                m.textArea.SetValue(m.currNote.Body)
+                m.textArea.Focus()
+                m.textArea.CursorEnd()
+                m.state = bodyView
+			}
+		}
+	}
+	return m, tea.Batch(cmds...)
+}
--- tui/view.go
+++ tui/view.go
@@ -1,5 +1,38 @@
 package tui
 
+import (
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+    appNameStyle = lipgloss.NewStyle().Background(lipgloss.Color("99")).Padding(0, 1)
+
+    faintStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("255")).Faint(true)
+
+    enumeratorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("99")).MarginRight(1)
+
+)
+
 func (m model) View() string {
-    return "NOTES APP"
+    s := appNameStyle.Render("NOTES APP") + "\n\n"
+
+    if m.state == listView {
+        for i, n := range m.notes {
+            prefix := " "
+            if i == m.listIndex {
+                prefix = ">"
+            }
+
+            shortBody := strings.ReplaceAll(n.Body, "\n", " ")
+            if len(shortBody) > 30 {
+                shortBody = shortBody[:30]
+            }
+            s += enumeratorStyle.Render(prefix) + n.Title + " | " + faintStyle.Render(shortBody)
+        }
+        s += faintStyle.Render("n - new note, q - quit")
+    }
+
+    return s
 }
